58e06b5d868c449e228d2572b43da28ccfa04dcc
git <saibalaji.polakampally@accoliteindia.com>
2017-04-14 11:24:59 +0530

    1) automated the process of logging in to the application

--- src/login.ts
+++ src/login.ts
@@ -1,9 +1,13 @@
 import { ElementFinder, browser, by, element } from 'protractor';
 
 describe ('login to tiwar', () => {
+    var userName;
+    var password;
 
     beforeAll(() => {
         browser.ignoreSynchronization = true;
+        userName = 'sample';
+        password = 'sample';
     });
 
     beforeEach(() => {
@@ -21,8 +25,29 @@ describe ('login to tiwar', () => {
         });
     });
 
+    function waitForPageLoad (seconds:number) {
+        browser.sleep(seconds*1000);
+    }
+
     it('Login', () => {
-        
+        let signInButton = element(by.xpath('//span[text()="Sign In"]'));
+        signInButton.click();
+        waitForPageLoad(2);
+        loggingIn ();
+        waitForPageLoad(5);
     });
+
+    function loggingIn () {
+        let userNameLabel = element(by.name('login'));
+        let passwordLabel = element(by.name('pass'));
+        let signInLabel = 'Sign In';
+        let signInXpath = "//input[@value='"+signInLabel+"']";
+        console.log("signIn Xpath is : " + signInXpath);
+        let signInButton = element(by.xpath(signInXpath));
+        userNameLabel.sendKeys(userName);
+        passwordLabel.sendKeys(password);
+        signInButton.click();
+        waitForPageLoad(5);
+    }
 
 });
